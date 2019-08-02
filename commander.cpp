11a6628270851a075fa33de5fdf659e3c769cc0b
Choi Shing Wan <choishingwan@gmail.com>
2019-08-02 14:13:46 -0400

    [Update] Add more detail log file

--- commander.cpp
+++ commander.cpp
@@ -72,9 +72,80 @@ bool Commander::initialize(int argc, char** argv)
         }
         opt = getopt_long(argc, argv, optString, longOpts, &longIndex);
     }
-    if (error) { throw std::runtime_error(error_message); }
     if (m_p_threshold.empty())
     { m_p_threshold = {5e-8, 5e-7, 5e-6, 5e-5, 5e-4, 0.005, 0.05, 0.5, 1}; }
+    if (m_eqtl_name.empty())
+    {
+        error = true;
+        error_message.append("Error: You must provide the eQTL file\n");
+    }
+    if (m_sumstat_name.empty())
+    {
+        error = true;
+        error_message.append(
+            "Error: You must provide the summary statistic file\n");
+    }
+    if (m_target_name.empty())
+    {
+        error = true;
+        error_message.append("Error: You must provide the target file\n");
+    }
+    if (m_msigdb_name.empty())
+    {
+        error = true;
+        error_message.append("Error: You must provide the MSigDB file\n");
+    }
+    if (m_gtf_name.empty())
+    {
+        error = true;
+        error_message.append("Error: You must provide the GTF file\n");
+    }
+    if (m_e_snp_id.empty())
+    {
+        error = true;
+        error_message.append(
+            "Error: You must provide the SNP column name in the eQTL file\n");
+    }
+    if (m_e_gene_id.empty())
+    {
+        error = true;
+        error_message.append(
+            "Error: You must provide the Gene column name in the eQTL file\n");
+    }
+    if (m_e_pvalue.empty())
+    {
+        error = true;
+        error_message.append("Error: You must provide the P-value column name "
+                             "in the eQTL file\n");
+    }
+    if (m_s_snp_id.empty())
+    {
+        error = true;
+        error_message.append("Error: You must provide the SNP column name in "
+                             "the summary statistic file\n");
+    }
+    if (m_out.empty())
+    {
+        error = true;
+        error_message.append("Error: You must provide the output file name\n");
+    }
+    if (error) { throw std::runtime_error(error_message); }
+
+    std::cerr << argv[0] << " \\" << std::endl;
+    std::cerr << "    --eGene " << m_e_gene_id << " \\" << std::endl;
+    std::cerr << "    --eP " << m_e_pvalue << " \\" << std::endl;
+    std::cerr << "    --eqtl " << m_eqtl_name << " \\" << std::endl;
+    std::cerr << "    --eSNP " << m_e_snp_id << " \\" << std::endl;
+    std::cerr << "    --gtf " << m_gtf_name << " \\" << std::endl;
+    std::cerr << "    --msigdb " << m_msigdb_name << " \\" << std::endl;
+    std::cerr << "    --out " << m_out << " \\" << std::endl;
+    std::cerr << "    --pthres " << m_p_threshold.front();
+    for (size_t i = 1; i < m_p_threshold.size(); ++i)
+    { std::cerr << "," << m_p_threshold[i]; }
+    std::cerr << std::endl;
+    std::cerr << "    --rsid " << m_s_snp_id << " \\" << std::endl;
+    std::cerr << "    --sumstat " << m_sumstat_name << " \\" << std::endl;
+    std::cerr << "    --target " << m_target_name << std::endl;
     return true;
 }
 void Commander::usage()
